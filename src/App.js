0807ca6e40e8ed3db8806d458cc4ca43c24a58ce
ADITI <153920845+ADITISONIC@users.noreply.github.com>
2024-11-07 18:24:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import RandomColor from './Components/Randomcolor';
+
+
+function App() {
+  return (
+    <div className="App">
+     <RandomColor/>
+    </div>
+  );
+}
+
+export default App;
